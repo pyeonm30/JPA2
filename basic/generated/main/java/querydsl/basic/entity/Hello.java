de65bca07186ae157bd7536ce3ceabe9bc1859b3
yeonmitc <82494535+yeonmitc@users.noreply.github.com>
2024-03-06 23:12:15 +0900

    ..
    .

---
+++ basic/generated/main/java/querydsl/basic/entity/Hello.java
@@ -0,0 +1,14 @@
+package querydsl.basic.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Hello {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
