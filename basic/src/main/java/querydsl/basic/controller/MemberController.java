7d3148fc3d7437c4ba11cc08eac3eebfc36befe0
yeonmitc <82494535+yeonmitc@users.noreply.github.com>
2024-03-21 07:52:05 +0900

    .
    .

---
+++ basic/src/main/java/querydsl/basic/controller/MemberController.java
@@ -0,0 +1,20 @@
+package querydsl.basic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import querydsl.basic.dto.MemberSearchCond;
+import querydsl.basic.dto.MemberTeamDto;
+import querydsl.basic.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCond condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
